503db3c0be8eeee0198dd7ae5f34aff968cd6793
fchemkct <hieu24a4@gmail.com>
2024-10-12 00:44:24 +1100

    Energy bar merged with inventory + UI adjustments
    adjusted energy bar to fit with full hd display; item harvest now costs energy per 'E' input

--- Assets/Scripts/Interactibles/ItemHarvestSource.cs
+++ Assets/Scripts/Interactibles/ItemHarvestSource.cs
@@ -10,12 +10,20 @@ public class ItemHarvestSource : MonoBehaviour
 {
     [SerializeField] Item itemResource;
     [SerializeField] int amountClicks = 1;
+    [SerializeField] int energyCost;
+    private EnergyManager EnergyMana;
+
+    void Start()
+    {
+        EnergyMana = GameObject.Find("EnergyManager").GetComponent<EnergyManager>();
+    }
 
     /*Maybe change it so that player can harvest multiples of item*/
     public Item HarvestResource()
     {
         amountClicks -= 1;
         if(amountClicks == 0) gameObject.SetActive(false);
+        EnergyMana.LoseEnergy(energyCost);
         Debug.Log($"Harvested Item: {itemResource.name}");
         return itemResource;
     }
