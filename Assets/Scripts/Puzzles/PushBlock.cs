6db4b8efa19c0c3272f7a5f121aef444dc8a9dc0
Nemo-5 <stickman5464@gmail.com>
2025-01-20 14:43:53 +1100

    Made Pushblock for future puzzles

---
+++ Assets/Scripts/Puzzles/PushBlock.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBlock : MonoBehaviour
+{
+    public float move_distance = 0.25f;
+    public float tween_time = 0.25f;
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Player")){
+            PushDirection(other.transform.position);
+        }
+    }
+
+    private void PushDirection(Vector3 playerPos){
+        //Calculate the direction to move first
+        double x_value = playerPos.x - transform.position.x;
+        double z_value = playerPos.z - transform.position.z;
+        bool is_x = Math.Abs(x_value) > Math.Abs(z_value);
+
+        double playerValue;
+        double blockValue;
+        Vector3 direction;
+        int dir;
+
+        if(is_x){
+            playerValue = playerPos.x;
+            blockValue = transform.position.x;
+            direction = new Vector3(1, 0, 0);
+        }
+        else{
+            playerValue = playerPos.z;
+            blockValue = transform.position.z;
+            direction = new Vector3(0, 0, 1);
+        }
+        
+        if(playerValue > blockValue)
+            dir = -1;
+        else
+            dir = 1;
+        
+        direction *= dir;
+        
+        //Move the block if it is not facing an obstacle.
+        if(!Physics.Raycast(transform.position, direction, move_distance))
+            LeanTween.move(gameObject, transform.position + direction * move_distance, tween_time);
+    }
+}
