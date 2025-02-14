46a32d720304691bb78c85c370d9538a7c3833b0
Alex Kam <kamzhanyue@gmail.com>
2025-02-15 01:21:56 +0800

    feat: add inventory popup items

--- Assets/Scripts/UI/InventoryPopup.cs
+++ Assets/Scripts/UI/InventoryPopup.cs
@@ -1,9 +1,11 @@
 using Kuroneko.UIDelivery;
+using UnityEngine;
 
 namespace UI
 {
     public class InventoryPopup : Popup
     {
+        [SerializeField] private InventoryPopupItem samplePopupItem;
         protected override void InitPopup()
         {
         
---
+++ Assets/Scripts/UI/InventoryPopupItem.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class InventoryPopupItem : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text title;
+    }
+}
