955d6112d2de67ab56c7a5d1c4f2fec018515c8a
Alex Kam <kamzhanyue@gmail.com>
2025-02-15 16:38:41 +0800

    wip: create dialogue popup scripts

---
+++ Assets/Scripts/UI/DialoguePopup.cs
@@ -0,0 +1,13 @@
+using Kuroneko.UIDelivery;
+using UnityEngine;
+
+namespace UI
+{
+    public class DialoguePopup : Popup
+    {
+        
+        protected override void InitPopup()
+        {
+        }
+    }
+}
