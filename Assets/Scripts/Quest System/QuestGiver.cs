f60b8f015ab5349c5396349b9ad0c84541558019
Bilguun <soproatlol@gmail.com>
2025-03-21 12:41:59 +1100

    chore: added a default formatter

--- Assets/Scripts/Quest System/QuestGiver.cs
+++ Assets/Scripts/Quest System/QuestGiver.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class QuestGiver : MonoBehaviour
 {
-    [SerializeField] private int questID; // the ID of the quest that this quest giver gives
-    
+    [SerializeField]
+    private int questID; // the ID of the quest that this quest giver gives
+
     public void GiveQuest()
     {
         QuestManager.instance.AddQuest(questID);
