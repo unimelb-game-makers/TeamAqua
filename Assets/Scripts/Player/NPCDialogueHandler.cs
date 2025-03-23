26da8fdb62f4814af397df6abfed71e141041d4f
fchemkct <hieu24a4@gmail.com>
2025-03-24 04:22:53 +1100

    bugfix:in progress:dialogue icons not showing up properly
    - dialogue icons can switch between quest icon and dialogue icon now.
    - method works with ink variables to switch icon. This is flexible if we want more than those 2 icons
    - currently messy, needs refractoring

--- Assets/Scripts/Player/NPCDialogueHandler.cs
+++ Assets/Scripts/Player/NPCDialogueHandler.cs
@@ -1,26 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Ink.Runtime;
 using UI;
 using UnityEngine;
 
 public class NPCDialogueHandler : MonoBehaviour
 {
-    [NonSerialized] public NPCDialogue dialogueSource = null;
-    [SerializeField] public UIController UIcontroller = null;
-
+    [NonSerialized] 
+    public NPCDialogue dialogueSource = null;
+    [SerializeField]
+    public NPCTag dialogueIcons;        //there are going to be many dialogue tags
+    [SerializeField] 
+    public UIController UIcontroller = null;
     // Update is called once per frame
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && dialogueSource != null && !DialogueSystem.GetIsPlaying() && !UIcontroller.pausePopup.isShowing /*&& not paused*/)
         {
             dialogueSource.PlayDialogue();
+            dialogueIcons.AttachStory();
         }
     }
     
     void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.CompareTag("Creature")) return;
+        dialogueIcons.CheckTag();
         if (other.gameObject.TryGetComponent(out NPC npc) && npc.dialogue)
         {
             dialogueSource = npc.dialogue;
