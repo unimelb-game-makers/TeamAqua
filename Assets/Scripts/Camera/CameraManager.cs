666ae0a4a8516f1435b0f29e376a40a3e1bdf389
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2025-05-04 14:23:12 +1000

    overhauling Noon Island

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager cameraManager;
+
+    public GameObject[] cameras;
+
+    public int currentCamera = 0;
+
+    private void Start() {
+        CameraManager.cameraManager = this;
+        SetCamera(currentCamera);
+    }
+
+    public void SetCamera(int idx){
+        currentCamera = idx;
+        for(int i = 0; i<cameras.Length; i++){
+            if(i == idx)
+                cameras[i].SetActive(true);
+            else
+                cameras[i].SetActive(false);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public int switchTo = 1;
+    public int switchBack = 0;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")){
+            CameraManager.cameraManager.SetCamera(switchTo);
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.CompareTag("Player")){
+            CameraManager.cameraManager.SetCamera(switchBack);
+        }
+    }
+}
