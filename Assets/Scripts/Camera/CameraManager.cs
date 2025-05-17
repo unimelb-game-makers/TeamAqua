4af22324970dbe1becd7dd3a3242a7c8e6d33be1
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2025-05-17 14:09:50 +1000

    Update Assets/Scripts/Camera/CameraManager.cs
    Co-authored-by: Alexander Kam <45383092+kam-zhan-yue@users.noreply.github.com>

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -10,7 +10,13 @@ public class CameraManager : MonoBehaviour
     public GameObject[] cameras;
 
     public int currentCamera = 0;
-
+private void Awake() {
+  if (instance != null && instance != this) {
+    Destroy(gameObject);
+  } else {
+    instance = this;
+  }
+}
     private void Start() {
         CameraManager.cameraManager = this;
         SetCamera(currentCamera);
