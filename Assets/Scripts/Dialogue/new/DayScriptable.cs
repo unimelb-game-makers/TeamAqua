8e8a2b01144a221150ac93b84fa352fec8f01649
kamzhanyue <kamzhanyue@gmail.com>
2025-07-04 21:58:38 +1000

    add day 0 object

--- Assets/Scripts/Dialogue/new/DayScriptable.cs
+++ Assets/Scripts/Dialogue/new/DayScriptable.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [Serializable]
 [CreateAssetMenu(menuName = "ScriptableObjects/Day Scriptable", fileName = "Day")]
 public class Day : ScriptableObject
 {
+    [InlineEditor]
     public DialoguePool dialoguePool;
     public List<Quest> mainQuests;
     public List<Quest> subQuests;
