0f17ce46348797c4a2ea7632fc6d798f81d2286d
fchemkct <hieu24a4@gmail.com>
2025-07-07 13:56:40 +0700

    fix:removed unused module

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Ink.Runtime;
-using Sirenix.OdinInspector;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
