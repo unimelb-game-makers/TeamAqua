0e676535eeb56737bc0515fe630e0b12eba6f391
fchemkct <hieu24a4@gmail.com>
2025-07-12 23:21:08 +0700

    fix:misplaced quest trigger for puz 1
    - it was misplaced,
    - i also tested taking multiple quests at once
    - also made more save templates for testing

--- Assets/Scripts/UI/QuestPopup.cs
+++ Assets/Scripts/UI/QuestPopup.cs
@@ -77,6 +77,8 @@ namespace Popups
             StringBuilder questBody = new();
 
             titleText.SetText(quest.title);
+
+            // quest details
             questBody.AppendLine(quest.description);
             for (int i = 0; i < quest.steps.Count; ++i)
             {
@@ -96,6 +98,7 @@ namespace Popups
             }
 
             questBody.AppendLine();
+            // quest rewards
             questBody.AppendLine("<color=green>Reward: " + "</color>");
             for (int j = 0; j < quest.reward.item.Count; ++j)
             {
