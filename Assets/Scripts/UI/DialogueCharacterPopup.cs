ea3d9c2a35c3506e8c9e170ff6bf89864c794278
fchemkct <hieu24a4@gmail.com>
2025-07-15 22:12:31 +0700

    feat:refractor tag code, more modularized

--- Assets/Scripts/UI/DialogueCharacterPopup.cs
+++ Assets/Scripts/UI/DialogueCharacterPopup.cs
@@ -52,61 +52,63 @@ namespace Popups
                 // handle the tags aside from quests
                 switch (tagKey)
                 {
-                    /*  --------------- might go unused, unsure if ID checking necessary or not
-                    case ID_TAG:        //check for ID at the top of each ink file and compare it to ID of quest
-                     id = int.Parse(tagValue); --> try to convert tagvalue to int
-                    break;
-                    */
-                    case SPEAKER_TAG: //change speaker name depending on the speaker tag
-                        // Noelle only apperas on the left
-                        // Other characters only appear on the right
-                        leftCharacter.gameObject.SetActiveFast(tagValue.Contains("Noelle"));
-                        leftCharacter.SetName(tagValue);
-                        rightCharacter.gameObject.SetActiveFast(
-                            tagValue != "Noelle" && tagValue != "Narrator"
-                        );
-                        rightCharacter.SetName(tagValue);
-
-                        DialogueManager
-                            .Instance()
-                            .DialogueAudioPlayer.SetCurrentAudioInfo(
-                                tagValue == "Narrator" ? "Narrator" : tagValue
-                            );
-
-                        /*
-                        if (PORTRAIT_TAG != null)
-                        {
-                            leftCharacter.PlayAnim(tagValue + "Idle");
-                        }*/
+                    case SPEAKER_TAG:
+                        SetSpeaker(tagValue);
                         break;
-
-                    case PORTRAIT_TAG: //change speaker portrait depending on portrait tag
-                        if (tagValue.Contains("Noelle"))
-                        {
-                            //leftCharacter.gameObject.SetActiveFast(true);
-                            leftCharacter.PlayAnim(tagValue);
-                        }
-                        else
-                            //rightCharacter.gameObject.SetActiveFast(true);
-                            rightCharacter.PlayAnim(tagValue);
+                    case PORTRAIT_TAG:
+                        SetPortrait(tagValue);
                         break;
                     case AUDIO_TAG:
-                        DialogueManager
-                            .Instance()
-                            .DialogueAudioPlayer.SetCurrentAudioInfo(tagValue);
+                        SetAudio(tagValue);
+                        break;
+                    case CUTSCENE_TAG:
+                        SetCutscene(tagValue);
                         break;
                     default:
-                        Debug.LogWarning(
-                            "tag came in but is not currently being handled: " + inkTag
-                        );
-                        break;
-                    case CUTSCENE_TAG: //change speaker portrait depending on portrait tag
-                        cutscene.gameObject.SetActiveFast(true);
-                        cutscene.Play(tagValue);
-                        Debug.Log("cutscene frame is " + tagValue);
+                        Debug.LogWarning("tag came in but is not being handled: " + inkTag);
                         break;
                 }
             }
+        }
+
+        public void SetSpeaker(string tagValue)
+        {
+            // Noelle only apperas on the left
+            // Other characters only appear on the right
+            leftCharacter.gameObject.SetActiveFast(tagValue.Contains("Noelle"));
+            leftCharacter.SetName(tagValue);
+            rightCharacter.gameObject.SetActiveFast(tagValue != "Noelle" && tagValue != "Narrator");
+            rightCharacter.SetName(tagValue);
+
+            DialogueManager
+                .Instance()
+                .DialogueAudioPlayer.SetCurrentAudioInfo(
+                    tagValue == "Narrator" ? "Narrator" : tagValue
+                );
+        }
+
+        public void SetAudio(string tagValue)
+        {
+            DialogueManager.Instance().DialogueAudioPlayer.SetCurrentAudioInfo(tagValue);
+        }
+
+        public void SetCutscene(string tagValue)
+        {
+            cutscene.gameObject.SetActiveFast(true);
+            cutscene.Play(tagValue);
+            Debug.Log("cutscene frame is " + tagValue);
+        }
+
+        public void SetPortrait(string tagValue)
+        {
+            if (tagValue.Contains("Noelle"))
+            {
+                //leftCharacter.gameObject.SetActiveFast(true);
+                leftCharacter.PlayAnim(tagValue);
+            }
+            else
+                //rightCharacter.gameObject.SetActiveFast(true);
+                rightCharacter.PlayAnim(tagValue);
         }
     }
 }
