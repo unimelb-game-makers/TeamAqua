d2013c9c6b6f658a30a0c5c3017e14682d0a362c
fchemkct <hieu24a4@gmail.com>
2025-07-15 23:37:51 +0700

    fix:delete unused modules

--- Assets/Scripts/UI/DialogueCharacterPopup.cs
+++ Assets/Scripts/UI/DialogueCharacterPopup.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using System.Linq;
 using Kuroneko.UIDelivery;
 using Kuroneko.UtilityDelivery;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Popups
 {
