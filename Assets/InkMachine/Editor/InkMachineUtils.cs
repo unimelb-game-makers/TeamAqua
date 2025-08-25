b6d65cb1294bc91b65cd1f9ff8c1dd7c62ea34b8
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2025-08-25 23:39:09 +1000

    ink script generation implemented

--- Assets/InkMachine/Editor/InkMachineUtils.cs
+++ Assets/InkMachine/Editor/InkMachineUtils.cs
@@ -2,6 +2,7 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace InkMachine{
     public class InkMachineUtils{
@@ -18,17 +19,50 @@ namespace InkMachine{
         }
 
         // Create linking ink file
-        public static void GenerateInkLink(List<Object> files, string path){
+        private static void GenerateInkLink(List<Object> files, string path){
             // Get the last 4 characters of path
             string fileName = path.Length >= 4 ? path.Substring(path.Length - 4) : path;
             string inkFilePath = $"{path}/{fileName}.ink";
-            File.WriteAllText(inkFilePath, "");
-            
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("INCLUDE ../../Globals/Globals.ink");
+            List<string> days = new List<string>(); // Make list of days - A5_S2_D1, A5_S2_D2, etc
+
+            // Write the include lines and make list of days
+            foreach(Object file in files){
+                if (IsInkFile(file)){
+                    string f_Name = file.name;
+                    sb.AppendLine($"INCLUDE {f_Name}.ink");
+                    days.Add(f_Name);
+                }
+            }
+            sb.AppendLine("// Variable Setup");
+            for(int i = 0; i < days.Count; i++){
+                sb.AppendLine($"CONST DIALOGUE_{i + 1} = \"{days[i]}\"");
+            }
+            sb.AppendLine("{");
+            for(int i = 0; i < days.Count; i++){
+                sb.AppendLine($"\t- dialogue_id == DIALOGUE_{i+1}:");
+                sb.AppendLine($"\t\t-> {days[i]}");
+            }
+            sb.AppendLine("}");
+            File.WriteAllText(inkFilePath, sb.ToString());
+
             AssetDatabase.Refresh();
         }
 
+        public static bool IsInkFile(Object obj)
+        {
+            if (obj == null) return false;
+            
+            string path = AssetDatabase.GetAssetPath(obj);
+            if (string.IsNullOrEmpty(path)) return false;
+            
+            return System.IO.Path.GetExtension(path).ToLower() == ".ink";
+        }
+
         // Copy each file in the file list to the target directory
-        public static void CopyFilesTo(List<Object> Files, string targetDir){
+        private static void CopyFilesTo(List<Object> Files, string targetDir){
             foreach(Object file in Files){
                 if (file == null) continue;
                 string sourcePath = AssetDatabase.GetAssetPath(file);
@@ -41,7 +75,7 @@ namespace InkMachine{
         }
 
         // Create a directory and return true and false whether it already exists
-        public static bool CreateDirectory(string path){
+        private static bool CreateDirectory(string path){
             if (!Directory.Exists(path)){
                 Directory.CreateDirectory(path);
                 AssetDatabase.Refresh();
